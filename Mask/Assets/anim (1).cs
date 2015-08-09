3ec27da1c19a39d2a931ae7bfe73054f999acced
tim chu <tim.chu90@gmail.com>
2015-08-09 12:51:41 -0400

    JSON file parser working.
    sample metadata and sample payload are parsed.

---
+++ Mask/Assets/anim (1).cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class anim : MonoBehaviour {
+	int x;
+	SkinnedMeshRenderer skinnedMeshRenderer;
+	// Use this for initialization
+	void Start () {
+		x = 0;
+		skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () { 
+		x = x + 1;
+		skinnedMeshRenderer.SetBlendShapeWeight(1, x);
+		skinnedMeshRenderer.SetBlendShapeWeight(2, x);
+	
+	}
+}
--- Mask/Assets/jsonParse.cs
+++ Mask/Assets/jsonParse.cs
@@ -1,8 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 using SimpleJSON;
+using System.Text;
+using System.IO;
+
 public class jsonParse : MonoBehaviour {
+	int x;
+	SkinnedMeshRenderer skinnedMeshRenderer;
+	string output;
 	string text = "";
+	/*
 	string s = 
 		"{\"ptype\":\"snippet\"," +
 			"\"text\":\"I trusted her\", "   +
@@ -15,12 +22,54 @@ public class jsonParse : MonoBehaviour {
 			"\"type\":\"statement\"," +
 			"\"time\":0," +
 			"\"emotions\":{\"anger\":0.5 , \"disgust\":0.6, \"happy\":0.0, \"sad\":0.3, \"scared\":0.0}}";
+	*/
+	private string Load(string fileName)
+	{
+		// Handle any problems that might arise when reading the text
+		string line;
+		// Create a new StreamReader, tell it which file to read and what encoding the file
+		// was saved as
+		StreamReader theReader = new StreamReader(fileName, Encoding.Default);
+		
+		// Immediately clean up the reader after this block of code is done.
+		// You generally use the "using" statement for potentially memory-intensive objects
+		// instead of relying on garbage collection.
+		// (Do not confuse this with the using directive for namespace at the 
+		// beginning of a class!)
+		using (theReader)
+		{
+			// While there's lines left in the text file, do this:
+			do
+			{
+				line = theReader.ReadLine();
+				
+				if (line != null)
+				{
+					// Do whatever you need to do with the text line, it's a string now
+					// In this example, I split it into arguments based on comma
+					// deliniators, then send that array to DoStuff()
+					output = string.Concat (output,line);
+				}
+			}
+			while (line != null);
+			
+			// Done reading, close the reader and return true to broadcast success    
+			theReader.Close();
+		}
+		return output;
+	}
 
 	// Use this for initialization
 	IEnumerator Start () {
+		x = 0;
+		skinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+		string metadata = Load ("Assets/SampleMetadata.JSON");
+		string s = Load ("Assets/SamplePayload.JSON");
+		//Debug.Log (metadata);
+		var metapacket = JSON.Parse (metadata);
 		yield return StartCoroutine (handler (s));
-		yield return new WaitForSeconds(5);
-		yield return StartCoroutine (handler (s1));
+		//yield return new WaitForSeconds(5);
+		//yield return StartCoroutine (handler (s1));
 	}
 
 	IEnumerator handler(string new_str){
@@ -60,6 +109,8 @@ public class jsonParse : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		x = x + 1;
+		skinnedMeshRenderer.SetBlendShapeWeight(1, x);
+		skinnedMeshRenderer.SetBlendShapeWeight(2, x);
 	}
 }
