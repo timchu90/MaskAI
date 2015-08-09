fcd7c979aab6c5e9c42d664c66bf02dc51e60dc7
tim chu <tim.chu90@gmail.com>
2015-08-09 15:09:49 -0400

    Metadata goal values storage and calculation

--- Mask/Assets/jsonParse.cs
+++ Mask/Assets/jsonParse.cs
@@ -7,7 +7,6 @@ using System.IO;
 public class jsonParse : MonoBehaviour {
 	int x;
 	SkinnedMeshRenderer skinnedMeshRenderer;
-	string output;
 	string text = "";
 	/*
 	string s = 
@@ -27,6 +26,7 @@ public class jsonParse : MonoBehaviour {
 	{
 		// Handle any problems that might arise when reading the text
 		string line;
+		string output = "";
 		// Create a new StreamReader, tell it which file to read and what encoding the file
 		// was saved as
 		StreamReader theReader = new StreamReader(fileName, Encoding.Default);
@@ -45,9 +45,7 @@ public class jsonParse : MonoBehaviour {
 				
 				if (line != null)
 				{
-					// Do whatever you need to do with the text line, it's a string now
-					// In this example, I split it into arguments based on comma
-					// deliniators, then send that array to DoStuff()
+					//concatenante each line to the output string
 					output = string.Concat (output,line);
 				}
 			}
@@ -66,31 +64,64 @@ public class jsonParse : MonoBehaviour {
 		string metadata = Load ("Assets/SampleMetadata.JSON");
 		string s = Load ("Assets/SamplePayload.JSON");
 		//Debug.Log (metadata);
-		var metapacket = JSON.Parse (metadata);
-		yield return StartCoroutine (handler (s));
+		yield return StartCoroutine (handler (s,metadata));
 		//yield return new WaitForSeconds(5);
 		//yield return StartCoroutine (handler (s1));
 	}
 
-	IEnumerator handler(string new_str){
+	IEnumerator handler(string new_str, string meta){
 		Debug.Log ("JSON PARSER");
 		var packet = JSON.Parse(new_str);
+		var metapacket = JSON.Parse(meta);
+
+		//JSON PACKET PARSING
 		var packetType = packet["ptype"].Value;
 		text = packet["text"].Value;
 		var type = packet["type"].Value;
 		var time = packet["time"].AsInt;
-		var anger = packet["emotions"]["anger"].AsFloat;
+
+		var angry = packet["emotions"]["anger"].AsFloat;
 		var disgust = packet["emotions"]["disgust"].AsFloat;
 		var happy = packet["emotions"]["happy"].AsFloat;
 		var sad = packet["emotions"]["sad"].AsFloat;
 		var scared = packet["emotions"]["scared"].AsFloat;
+		float[] emotions = new float[5];
+		emotions [0] = happy;
+		emotions [1] = sad;
+		emotions [2] = angry;
+		emotions [3] = scared;
+		emotions [4] = disgust;
+		float[] blendshapes = new float[20];
+
 		Debug.Log ("text:" + text + 
 		           " time:" + time + 
-		           " anger:" + anger + 
+		           " angry:" + angry + 
 		           " disgust:" + disgust + 
 		           " happy:" + happy + 
 		           " sad:" + sad + 
 		           " scared:" + scared + "\n");
+
+		//JSON METADATA PARSING
+		//get all weight values of every key and average the non-zero values
+		for (int j = 8; j < 28; j++) {
+			blendshapes[j-8] = 0; //metadata file starts at 8 to skip initial name,id,age etc. metadata
+			var count = 0;
+			for (int i = 0; i < 5; i++) {
+				if(metapacket ["profiles"] [0] [j] [i].AsFloat * emotions[i] > 0){
+					blendshapes[j-8] = blendshapes[j-8] + (metapacket ["profiles"] [0] [j] [i].AsFloat * emotions[i]);
+					count++;
+				}
+			}
+			if(count >0){
+				blendshapes[j-8] = blendshapes[j-8] / count;
+			}
+		}
+
+		//debug log code
+		for (int i = 0; i < 20; i++) {
+			Debug.Log (blendshapes [i]);
+		}
+
 		Audio audio = new Audio ();
 		AudioSource source = gameObject.GetComponent<AudioSource> ();
 		yield return StartCoroutine(audio.Play( text, false , source));
